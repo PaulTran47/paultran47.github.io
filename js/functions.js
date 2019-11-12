8dec3497037a18afabe761fc2d33d27e3b3577a0
Paul Le Tran (Trần Lê Anh Phú) <gwong.lee@gmail.com>
2019-11-12 01:27:52 -0500

    Testing conversion of more images to webp

--- js/functions.js
+++ js/functions.js
@@ -28,3 +28,29 @@ function openMenu() {
   $menuList.removeClass('hidden');
   $menuOverlay.removeClass('hidden');
 }
+
+function setBackgroundIMG() {
+  var $imgSection = $('.row-img');
+
+  // Loop through all the myclass elements
+  $imgSection.each(function () {
+    // Find the image tag within the myclass
+    var $img = $(this).find('img');
+
+    // Get the image the browser will use in the picture tag
+    var $srcSet = $img.prop('currentSrc') || $img.prop('src');
+
+    // For debug purpose
+    console.log($srcSet);
+
+    // Set the myclass background 
+    $(this).css({
+      'background-image': 'url(' + $srcSet + ')'
+    });
+
+  });
+}
+
+$(window).on('load resize', function () {
+  setBackgroundIMG();
+});
