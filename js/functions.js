c5f3d28b950443dc5ed073e0f78f2a892801abd0
Paul Le Tran (Trần Lê Anh Phú) <gwong.lee@gmail.com>
2021-10-17 05:15:48 -0500

    Testing lazy loading image process.

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+/* Functionality for mobile menu */
 let $menuOpenIcon = document.querySelector('.icon-menu'),
   $menuCloseIcon = document.querySelector('.icon-menu-close'),
   $menuList = document.querySelector('.site-menu'),
@@ -26,4 +27,27 @@ function openMenu() {
   $menuList.classList.remove("hidden");
   $menuList.classList.remove("hidden");
   $menuOverlay.classList.remove("hidden");
+}
+
+/* InntersectionObserver API */
+const imgObserver = new IntersectionObserver((entries, self) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      lazyLoad(entry.target);
+      self.unobserve(entry.target);
+    }
+  });
+});
+
+document.querySelectorAll('.lazy-img').forEach(img => {
+  imgObserver.observe(img);
+});
+
+/* Function that lazy loads */
+function lazyLoad(img) {
+  const picture = img.parentElement;
+  const source = picture.querySelector('.lazy-source');
+
+  source.srcset = source.getAttribute('data-srcset');
+  img.src = img.getAttribute('data-src');
 }
