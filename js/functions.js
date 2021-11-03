b1a51859a19c96bfb6c178a95c6a240f37c6a6e5
Paul Le Tran (Trần Lê Anh Phú) <gwong.lee@gmail.com>
2021-11-02 22:12:04 -0500

    Added filtered live keyword search to more page
    Removed more unnecessary div tags for semantic HTML.
    
    Fixed section spacers such that filtered search results won't have awkward spacers on top and below them.

--- js/functions.js
+++ js/functions.js
@@ -7,8 +7,8 @@ if(menuOverlay) {
   menuOverlay.addEventListener('click', function(e){
     closeMenu();
   });
-  menuOverlay.addEventListener("keydown", function(event) {
-    if (event.key === "Escape") {
+  menuOverlay.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
       closeMenu();
     }
   });
@@ -17,15 +17,15 @@ if(menuCloseIcon) {
   menuCloseIcon.addEventListener('click', function(e){
     closeMenu();
   });
-  menuCloseIcon.addEventListener("keydown", function(event) {
-    if (event.key === "Escape") {
+  menuCloseIcon.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
       closeMenu();
     }
   });
 }
 if(menuList) {
-  menuList.addEventListener("keydown", function(event) {
-    if (event.key === "Escape") {
+  menuList.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
       closeMenu();
     }
   });
@@ -44,10 +44,10 @@ function closeMenu(){
   menuOverlay.classList.add('hidden');
 }
 function openMenu() {
-  menuCloseIcon.classList.remove("hidden");
-  menuList.classList.remove("hidden");
-  menuList.classList.remove("hidden");
-  menuOverlay.classList.remove("hidden");
+  menuCloseIcon.classList.remove('hidden');
+  menuList.classList.remove('hidden');
+  menuList.classList.remove('hidden');
+  menuOverlay.classList.remove('hidden');
 }
 /* InntersectionObserver API */
 const imgObserver = new IntersectionObserver((entries, self) => {
@@ -72,9 +72,9 @@ function lazyLoad(img) {
   Filtered live search function with ARIA22 accessibility.
   Search is case-insensitive, whitespace-senstive.
 */
-let cards = document.querySelectorAll('.callout-box');
+let cards = document.querySelectorAll('[aria-label="Callout box"]');
 function liveSearch() {
-  let searchQuery = document.getElementById("searchbox").value;
+  let searchQuery = document.getElementById('searchbox').value;
   let isEmpty
   /*
     Use innerText if all contents are visible
@@ -83,23 +83,23 @@ function liveSearch() {
   */
   let counter = 0;
   for (var i = 0; i < cards.length; i++) {
-    if(cards[i].textContent.toLowerCase().includes(searchQuery.toLowerCase())) {
-      cards[i].classList.remove("is-hidden");
+    if(cards[i].innerText.toLowerCase().includes(searchQuery.toLowerCase())) {
+      cards[i].classList.remove('hide');
       counter += 1;
     }
     /* If input field is empty or has only whitespaces, display all results (i.e., no filter applied) */
-    else if (searchQuery == "" || searchQuery == null || searchQuery.trim().length == 0) {
-      cards[i].classList.remove("is-hidden");
+    else if (searchQuery == '' || searchQuery == null || searchQuery.trim().length == 0) {
+      cards[i].classList.remove('hide');
     }
     else {
-      cards[i].classList.add("is-hidden");
+      cards[i].classList.add('hide');
     }
-    if (searchQuery != "" && searchQuery != null && searchQuery.trim().length != 0) {
-      document.getElementById("resultsmsg").innerHTML = counter + ' publication(s) found.';
+    if (searchQuery != '' && searchQuery != null && searchQuery.trim().length != 0) {
+      document.getElementById('resultsmsg').innerHTML = counter + ' result(s) found.';
     }
     /* Return default ARIA22 message. */
     else {
-      document.getElementById("resultsmsg").innerHTML = 'Publication&#0040;s&#0041; with matched keywords will be displayed in real-time.';
+      document.getElementById('resultsmsg').innerHTML = 'Result&#0040;s&#0041; with matched keywords will be displayed in real-time.';
     }
   }
 }
